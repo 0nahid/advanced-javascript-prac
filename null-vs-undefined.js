8b0dfee59316599320aa8da4453c4ed721518f17
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-02 00:08:10 +0600

    null vs undefined interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+    // console.log(num1 + num2);
+    return num1 + num2;
+}
+const result = add(13, 12);
+console.log(result);
+
+function sum(num1, num2) {
+    console.log(num1, num2);
+}
+const result2 = sum(15);
+console.log(result2);
+
+const premik = {name:'cool boy Rohi Ullah', phone:'12655416541'}
+console.log('premik',premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages =[10,12,15,12,16,23]
+console.log(ages[11]);
