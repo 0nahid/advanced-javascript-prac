84f182cd492dce0e9f757929682f731f7fec9369
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-02 20:35:14 +0600

    scope js

---
+++ scope.js
@@ -0,0 +1,15 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + 20;
+    console.log(result);
+    if (result > 9) {
+        let mood = 'Happy';
+        mood = 'funky'
+        console.log(mood);
+    }
+    return result;
+}
+const output = sum(10, 12);
+console.log(bonus);
+console.log(output);
