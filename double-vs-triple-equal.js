6911a2e91dcad2b584ed515906e63c38bda00e4a
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-02-02 15:59:48 +0600

    double vs tripple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,26 @@
+let first = 1;
+let second = true;
+if (first == second) {
+    console.log('condition true');
+} else {
+    console.log('condition is false');
+}
+
+let first2 = 0;
+let second2 = false;
+if (first2 == second2) {
+    console.log('condition true');
+} else {
+    console.log('condition is false');
+}
+// == value check
+// === check type
+
+const first3 = 2;
+const second3 = '2';
+if (first3 === second3) {
+    console.log('condition true');
+} else {
+    console.log('condition is false');
+}
+
